2a210a2000dbe578578c06c3aa991196a168d37f
Kyryl <pozniak17kirill@gmail.com>
2026-04-23 12:45:18 +0300

    add password on swager

--- api/Program.cs
+++ api/Program.cs
@@ -27,6 +27,47 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
+// логин/пароль на свагер
+var swaggerUser = Environment.GetEnvironmentVariable("SWAGGER_USER");
+var swaggerPass = Environment.GetEnvironmentVariable("SWAGGER_PASS");
+
+app.Use(async (context, next) =>
+{
+    var path = context.Request.Path.Value;
+
+    if (path.StartsWith("/swagger"))
+    {
+        string authHeader = context.Request.Headers["Authorization"];
+
+        if (authHeader != null && authHeader.StartsWith("Basic "))
+        {
+            var encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+            var decoded = System.Text.Encoding.UTF8.GetString(
+                Convert.FromBase64String(encodedUsernamePassword)
+            );
+
+            var parts = decoded.Split(':');
+
+            var username = parts[0];
+            var password = parts[1];
+
+            if (username == swaggerUser && password == swaggerPass)
+            {
+                await next();
+                return;
+            }
+        }
+
+        context.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Swagger\"";
+        context.Response.StatusCode = 401;
+        return;
+    }
+
+    await next();
+});
+
+
+
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
